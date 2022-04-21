3db63f7401ec80eb113ec44f197f9d2144498fa4
David Martinez <martinez.claudiodavid@gmail.com>
2022-04-21 18:23:30 -0300

    cuartaentrega

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ItemListContainer from './Components/ItemListContainer'
+import ItemCount from './ItemCount'
+
 
 const saludos= "Hola a todos , Bienvenidos"
+const onAdd="no hay stock"
+
+
 const Main = () => {
+
+  
   return (
     <>
     <ItemListContainer greeting={saludos}/> 
+   <ItemCount stock={5} inicial={0} onAdd={onAdd} />
     </>
   )
 }
